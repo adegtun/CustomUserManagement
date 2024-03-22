b424be2be57829871b9a558911c482a767cca639
Temitope <adegtun@yahoo.com>
2024-03-22 11:11:25 +0100

    Initial Commit

---
+++ CustomUserManagement/Migrations/20240322082003_Added Custom Properties.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomUserManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCustomProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
